e1893f21d3d32b7c32a8fc7191d5f2da45ccad2f
Jonie <joniebariaDE@gmail.com>
2023-03-18 06:53:38 +0800

    heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const request = require("request")
+
+
+
+app.use(express.static("static"));
+
+app.get("/", function(req, res){
+    res.send("Hello");
+});
+
+app.get("/contact", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server started on port 3000!")
+});
